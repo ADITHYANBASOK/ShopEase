261087f3fcbd7cfe7b168044999b0c4c170ed715
Adithyan B Asok <adithyanbasok@gmail.com>
2024-03-28 15:51:35 +0530

    Navbar Modified

--- client/src/Component/Navbar.jsx
+++ client/src/Component/Navbar.jsx
@@ -16,7 +16,7 @@ function Navbar() {
                             <div className="collapse navbar-collapse justify-content-center" id="navbarTogglerDemo01">
                                 <ul className="navbar-nav mb-2 mb-lg-0">
                                     <li className="nav-item">
-                                        <NavLink exact activeClassName="active" className="nav-link" to="/">ALL</NavLink>
+                                        <NavLink exact activeClassName="active" className="nav-link" to="/">All</NavLink>
                                     </li>
                                     <li className="nav-item">
                                         <NavLink activeClassName="active" className="nav-link" to="/categories">Categories</NavLink>
